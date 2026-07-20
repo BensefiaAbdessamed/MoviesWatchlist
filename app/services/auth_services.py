64857a67d38401cdaa30d920aacbb4a286493386
BensefiaAbdessamed <pa_bensefia@esi.dz>
2026-07-21 00:44:49 +0100

    added a logout and token revocation services

--- app/services/auth_services.py
+++ app/services/auth_services.py
@@ -1,14 +1,15 @@
+from fastapi.responses import JSONResponse
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 import jwt
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 from app.core.config import settings
-from app.database.session import get_db
 from app.exceptions.auth_exceptions import NotAuthenticatedError
 from app.exceptions.user_exceptions import AlreadyExists
+from app.models import database
 from app.models.database import User
 from sqlalchemy import select
-from fastapi import Depends, HTTPException, Request, Response
+from fastapi import HTTPException, Request, Response
 from app.schemas import user
 from app.core.auth import create_access_token, create_refresh_token, hash_password, verify_password, verify_refresh_token
 
@@ -70,7 +71,7 @@ async def register(
         username = user.username,
         email = user.email.lower(),
         password_hash = hashed_pass,
-        role = user.role,
+        role = database.Role.USER
     )
 
     db.add(user)
@@ -101,6 +102,7 @@ async def register(
 
 async def refresh(
   request: Request,
+  response: Response,
   db: AsyncSession
 ):
     #   extract the token from cookie
@@ -116,7 +118,7 @@ async def refresh(
             settings.secret_key.get_secret_value(),
             algorithms=[settings.algorithm]
         )
-    except jwt.PyJWTError:
+    except jwt.InvalidTokenError:
         raise NotAuthenticatedError()
     else:
         user_id = decoded_token.get("sub")
@@ -124,15 +126,53 @@ async def refresh(
     #   validate credentials
     if not user_id:
         raise NotAuthenticatedError()
-    await verify_refresh_token(int(user_id), db) 
 
+    result = await db.execute(select(User).filter(User.id == int(user_id)))
+    user = result.scalar_one()
+
+    if user.refresh_token != token:
+        raise NotAuthenticatedError()
+    
     #   create new access/refresh token
     access_token = create_access_token(data={"sub": str(user_id)})
+    refresh_token = create_refresh_token(data={"sub": str(user_id)})
+
+    user.refresh_token = refresh_token
+    await db.commit()
+    await db.refresh(user)
+
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=False,
+        samesite="lax",
+        max_age=60*60*24*14,
+        path="/auth/refresh"
+    )
 
     #   return new acess token
     return {"access_token": access_token, "token_type": "bearer"}
-    
 
+async def logout(
+  response: Response,
+  db: AsyncSession,
+  user: User
+):
+    response.delete_cookie(
+        key="refresh_token",
+        path="/auth/refresh",
+    )
+
+    result = await db.execute(select(User).filter(User.id == user.id))
+    found_user = result.scalar_one()
+    found_user.refresh_token = None
+
+    return {
+        "staus code" : 200,
+        "detail" : "logged out successfully"
+    }
+    
             
 
     
